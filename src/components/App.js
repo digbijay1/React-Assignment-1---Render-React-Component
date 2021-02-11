74a2dbae01a78d9c48e7be6652355dfe1bae8c5b
digbijay1 <digbijay.das@suiit.ac.in>
2021-02-12 00:13:26 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,9 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="root">
-      <p>
-        I am learning React. My life is getting better. 
-      </p>
+      <p>I am learning React. My life is getting better. </p>
+
+
     </div>
   )
 }
